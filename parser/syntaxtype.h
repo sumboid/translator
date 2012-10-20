9ec5865f56036cd48f0c0a4f7c10847654955680
sumboid <ilya.sumb@gmail.com>
2012-10-20 22:17:03 +0700

    ops

---
+++ parser/syntaxtype.h
@@ -0,0 +1,17 @@
+#ifndef SYNTAXTYPE_H
+#define SYNTAXTYPE_H
+
+#include <string>
+
+namespace syntaxtype
+{
+    const char* CONST = "constant";
+    const char* ADD = "add";
+    const char* SUB = "sub";
+    const char* MUL = "mul";
+    const char* DIV = "div";
+    const char* POW = "pow";
+    const char* DIV = "div";
+}
+
+#endif /* end of include guard: SYNTAXTYPE_H */
